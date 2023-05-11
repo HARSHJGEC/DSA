1e4da6fa6b5cd89b9ebcc004270ab0766cdbaeec
Harsh Jha <58074534+HARSHJGEC@users.noreply.github.com>
2023-05-11 12:29:51 +0530

    Add files via upload

--- Chaining_Implementation.py
+++ Chaining_Implementation.py
@@ -3,10 +3,7 @@ HashTable = [[]for _ in range(10)]
 #Hashing function to return value for every key
 def Hashing(key):
     return key%len(HashTable)
-#Insert values
-def insert(HashTable,key):
-    hash_key=Hashing(key);
-    HashTable[hash_key].append(key);
+
 #display values
 def display_hash(hashTable):
       
@@ -18,7 +15,20 @@ def display_hash(hashTable):
             print(j, end = " ")
               
         print()
-# Driver code
+#search values
+def search_hash(hashTable, key):
+    for i in range(len(hashTable)):
+        for j in hashTable[i]:
+            if j==key:
+                return True
+    return False
+#Insert values
+def insert(HashTable,key):
+    if search_hash(HashTable,key)==False :
+        hash_key=Hashing(key);
+        HashTable[hash_key].append(key);
+    
+
 # Driver Code
 insert(HashTable, 10)
 insert(HashTable, 25)
@@ -26,5 +36,7 @@ insert(HashTable, 20)
 insert(HashTable, 9)
 insert(HashTable, 21)
 insert(HashTable, 21)
+if search_hash(HashTable,1):
+    print("10 availiable")
   
 display_hash (HashTable)
